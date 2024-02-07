092b45510d253f58f769110ed7240b2e9ad638ae
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-02-07 15:16:25 +0530

    In Progress-Edit Doctype (Suspend Enrollment Request) Edit Student Information HTML Field.

--- psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.js
+++ psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.js
@@ -2,7 +2,27 @@
 // For license information, please see license.txt
 
 // Custom functions
-function get_psa_student(student_name, callback) {
+function get_program(program, callback) {
+    frappe.call({
+        method: 'frappe.client.get_value',
+        args: {
+            doctype: 'Program',
+            filters: {
+                name: program
+            },
+            fieldname: ['college', 'department', 'specialization']
+        },
+        callback: function(response) {
+            var college = response.message.college;
+            var department = response.message.department;
+            var specialization = response.message.specialization;
+            callback(college, department, specialization);
+        }
+    });
+}
+
+
+function get_psa_student(creation_date, student_name, program, callback) {
     frappe.call({
         method: 'frappe.client.get_value',
         args: {
@@ -15,10 +35,14 @@ function get_psa_student(student_name, callback) {
         callback: function(response) {
             var full_name_arabic = response.message.full_name_arabic;
             var full_name_english = response.message.full_name_english;
-            callback(full_name_arabic, full_name_english);
+
+            get_program(program, function(college, department, specialization) {
+                callback(full_name_arabic, full_name_english, creation_date, program, college, department, specialization);
+            });
         }
     });
 }
+
 
 function get_year_of_enrollment(frm, callback) {
     frappe.call({
@@ -28,19 +52,19 @@ function get_year_of_enrollment(frm, callback) {
             filters: {
                 name: frm.doc.program_enrollment
             },
-            fieldname: ['creation', 'student']
+            fieldname: ['creation', 'program', 'student']
         },
         callback: function(response) {
             var creation_date = response.message.creation;
             var student_name = response.message.student;
+            var program = response.message.program;
 
-            get_psa_student(student_name, function(full_name_arabic, full_name_english) {
-                callback(creation_date, full_name_arabic, full_name_english);
+            get_psa_student(creation_date, student_name, program, function(full_name_arabic, full_name_english, creation_date, program, college, department, specialization) {
+                callback(creation_date, full_name_arabic, full_name_english, program, college, department, specialization);
             });
         }
     });
 }
-
 
 
 frappe.ui.form.on("Suspend Enrollment Request", {
@@ -50,11 +74,20 @@ frappe.ui.form.on("Suspend Enrollment Request", {
 
     program_enrollment(frm) {
         if (frm.doc.program_enrollment) {
-            get_year_of_enrollment(frm, function(creation_date, full_name_arabic, full_name_english) {
+            get_year_of_enrollment(frm, function(creation_date, full_name_arabic, full_name_english, program, college, department, specialization) {
                 var year_of_enrollment = new Date(creation_date).getFullYear();
-
-                $(frm.fields_dict["student_html"].wrapper).html('<span style="color: black;">Year of Enrollment: ' + year_of_enrollment + '<br>' + full_name_arabic + '<br>' + full_name_english + '</span>');
+                $(frm.fields_dict["student_html"].wrapper).html('<span style="color: black;">' +
+                __("Full Name Arabic") + ': ' + full_name_arabic + '<br>' +
+                __("Full Name English") + ': ' + full_name_english + '<br>' +
+                __("Year of Enrollment") + ': ' + year_of_enrollment + '<br>' +
+                __("Program") + ': ' + program + '<br>' +
+                __("College") + ': ' + college + '<br>' +
+                __("Department") + ': ' + department + '<br>' +
+                __("Specialization") + ': ' + specialization + '</span>');
             });
+        }
+        else {
+            $(frm.fields_dict["student_html"].wrapper).html('');
         }
     },
 });
