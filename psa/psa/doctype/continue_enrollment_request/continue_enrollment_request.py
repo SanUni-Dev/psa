dce6b0945afdca089fcf6e71c3df32ada59a42f4
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-03-18 06:59:23 +0530

    Edit: move api method that get last approved request to doc py.

--- psa/psa/doctype/continue_enrollment_request/continue_enrollment_request.py
+++ psa/psa/doctype/continue_enrollment_request/continue_enrollment_request.py
@@ -45,6 +45,19 @@ class ContinueEnrollmentRequest(Document):
 			):
 				frappe.throw(_("You can't add a continue enrollment request, because you have active request that is pending approval!"))
 
+	@frappe.whitelist(allow_guest=True)
+	def get_last_approved_suspend_enrollment_request(self, program_enrollment):
+		# Query to fetch the last document with status containing "Approved" and matching program_enrollment
+		docs = frappe.get_all("Suspend Enrollment Request",
+			fields=["*"],
+			filters={
+				"status": ["like", "%Approved%"],
+				"program_enrollment": program_enrollment
+			},
+			order_by="modified DESC",
+			limit_page_length=1
+		)
+		return docs[0] if docs else None
 
 
 	# @frappe.whitelist()
