2cf6b80e562558a60c20a0bd99ee61e4b813f9fc
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-03-28 00:02:50 +0530

    Fix: fix error in doctype timeline child table (In Progress).

--- psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.js
+++ psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.js
@@ -15,9 +15,13 @@ frappe.ui.form.on("Suspend Enrollment Request", {
         }, 500);
 
         $(frm.fields_dict["timeline_html"].wrapper).html('');
+        frm.set_df_property("timeline_section", "hidden", true);
 
         if (!frm.is_new()) {
-            format_timeline_html(frm, "timeline_html", frm.doc.timeline_child_table);
+            if (frm.doc.timeline_child_table && frm.doc.timeline_child_table.length > 0) {
+                format_timeline_html(frm, "timeline_html", frm.doc.timeline_child_table);
+                frm.set_df_property("timeline_section", "hidden", false);
+            }
             
             if (frm.doc.fees_status == "Not Paid") {
                 frm.set_intro((__(`You have to pay fees of request before confirm it!`)), 'red');
@@ -110,14 +114,13 @@ frappe.ui.form.on("Suspend Enrollment Request", {
         modified_of_before_workflow_action = frm.doc.modified.split(" ")[0] + " " + (frm.doc.modified.split(" ")[1]).split(".")[0];
     },
 
-    after_workflow_action(frm) {
-        var current_role_of_workflow_action = "";
-        // var current_role_of_workflow_action = frappe.user_roles[0];
+    async after_workflow_action(frm) {
+        var current_role_of_workflow_action = frappe.user_roles[0];
         var current_user_of_workflow_action = frappe.session.user_fullname;
         var status_of_after_workflow_action = frm.doc.status;
         var modified_of_after_workflow_action = frm.doc.modified.split(" ")[0] + " " + (frm.doc.modified.split(" ")[1]).split(".")[0];
 
-        var new_timeline = frappe.model.add_child(frm.doc, 'timeline_child_table');
+        var new_timeline = await frappe.model.add_child(frm.doc, 'timeline_child_table');
 
         new_timeline.position = current_role_of_workflow_action;
         new_timeline.full_name = current_user_of_workflow_action;
@@ -126,7 +129,6 @@ frappe.ui.form.on("Suspend Enrollment Request", {
         new_timeline.action = action_of_workflow;
         new_timeline.next_status = status_of_after_workflow_action;
         new_timeline.action_date = modified_of_after_workflow_action;
-
         if (action_of_workflow.includes('Reject') || action_of_workflow.includes('Approve')) {
             frm.save('Submit');
         }
