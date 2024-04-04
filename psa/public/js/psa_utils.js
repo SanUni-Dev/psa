2a6b6e0bacd89d77a6ee68d8bf1ec69a6750cf91
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-04-05 01:08:06 +0530

    Feat(Suspend): complete moving js functions that are common used in all doctypes to reduce declaring in every doctype and delete them from doctype files.

--- psa/public/js/psa_utils.js
+++ psa/public/js/psa_utils.js
@@ -182,3 +182,66 @@ psa_utils.get_active_request = function (doctype_name, program_enrollment, callb
         }
     });
 }
+
+
+psa_utils.get_program_enrollment = function (program_enrollment, callback) {
+    frappe.call({
+        method: 'frappe.client.get_value',
+        args: {
+            doctype: 'Program Enrollment',
+            filters: {
+                name: program_enrollment
+            },
+            fieldname: ['status', 'creation', 'program', 'student']
+        },
+        callback: function (response) {
+            var status = response.message.status;
+            var creation = response.message.creation;
+            var student = response.message.student;
+            var program = response.message.program;
+            
+            callback(status, creation, student, program);
+        }
+    });
+}
+
+
+psa_utils.get_program = function (program, callback) {
+    frappe.call({
+        method: 'frappe.client.get_value',
+        args: {
+            doctype: 'Program',
+            filters: {
+                name: program
+            },
+            fieldname: ['college', 'department', 'specialization']
+        },
+        callback: function (response) {
+            var college = response.message.college;
+            var department = response.message.department;
+            var specialization = response.message.specialization;
+
+            callback(college, department, specialization);
+        }
+    });
+}
+
+
+psa_utils.get_psa_student = function (student, callback) {
+    frappe.call({
+        method: 'frappe.client.get_value',
+        args: {
+            doctype: 'PSA Student',
+            filters: {
+                name: student
+            },
+            fieldname: ['full_name_arabic', 'full_name_english']
+        },
+        callback: function (response) {
+            var full_name_arabic = response.message.full_name_arabic;
+            var full_name_english = response.message.full_name_english;
+
+            callback(full_name_arabic, full_name_english);
+        }
+    });
+}
