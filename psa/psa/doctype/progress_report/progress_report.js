8274cdb8de33c09da7ffe6725214a59a707d1ea3
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-05-04 07:17:56 +0530

    Feat(Progress Report, Meeting, Publication Conditions, Proficiency Test): create doctypes with its fields and its child tables.

---
+++ psa/psa/doctype/progress_report/progress_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Sana'a university and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Progress Report", {
+// 	refresh(frm) {
+
+// 	},
+// });
