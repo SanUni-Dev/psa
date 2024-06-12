a975eeea07c9db8caa50927e9059cdd439a45fd4
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-06-12 06:26:04 +0530

    Faet(Suspend): added code to check active request using a dynamic function in js and in py 'Not Completed'.

--- psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.py
+++ psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.py
@@ -4,7 +4,7 @@
 import frappe, json
 from frappe.model.document import Document
 from frappe import _
-from psa.api.psa_utils import get_active_request
+from psa.api.psa_utils import get_active_request, check_active_request
 from frappe.utils import add_days, add_months, today, now_datetime, get_datetime
 
 class SuspendEnrollmentRequest(Document):
@@ -23,64 +23,77 @@ class SuspendEnrollmentRequest(Document):
             frappe.throw(_("Failed! Student is withdrawn!"))
 
     def before_insert(self):
-        program_enrollment_status = frappe.get_doc('Program Enrollment', self.program_enrollment)
+        active_request = check_active_request(
+            self.student,
+            self.program_enrollment,
+            ["Suspend Enrollment Request", "Continue Enrollment Request", "Withdrawal Request"]
+        )
+        if active_request:
+            url_of_active_request = '<a href="/app/{0}/{1}" title="{2}">{3}</a>'.format((active_request[0]).lower().replace(" ", "-"), active_request[1]['name'], _("Click here to show request details"), active_request[1]['name'])
+            frappe.throw(
+                _("Can't add a suspend enrollment request, because you have an active {0} (").format(active_request[0]) +
+                url_of_active_request +
+                _(") that is {0}!").format(active_request[1]['status'])
+            )
 
-        if program_enrollment_status.status == "Suspended":
-            url_of_continue_enrollment_request = frappe.utils.get_url_to_form('Continue Enrollment Request', "new")
-            frappe.throw(_("Can't add a suspend enrollment request, because current status is suspended!") + "<br><br><a href='" + url_of_continue_enrollment_request + "'>" + _('Do you want to add a continue enrollment request?') + "</a>")
+        # program_enrollment_status = frappe.get_doc('Program Enrollment', self.program_enrollment)
 
-        elif program_enrollment_status.status == "Withdrawn":
-            frappe.throw(_("Can't add a suspend enrollment request, because current status is withdrawn!"))
+        # if program_enrollment_status.status == "Suspended":
+        #     url_of_continue_enrollment_request = frappe.utils.get_url_to_form('Continue Enrollment Request', "new")
+        #     frappe.throw(_("Can't add a suspend enrollment request, because current status is suspended!") + "<br><br><a href='" + url_of_continue_enrollment_request + "'>" + _('Do you want to add a continue enrollment request?') + "</a>")
 
-        else:
-            suspend_set_a_limit_on_the_number_of_requests = frappe.db.get_single_value('PSA Settings', 'suspend_set_a_limit_on_the_number_of_requests')
-            suspend_number_of_requests = frappe.db.get_single_value('PSA Settings', 'suspend_number_of_requests')
+        # elif program_enrollment_status.status == "Withdrawn":
+        #     frappe.throw(_("Can't add a suspend enrollment request, because current status is withdrawn!"))
 
-            suspend_set_a_limit_on_the_number_of_rejected_requests = frappe.db.get_single_value('PSA Settings', 'suspend_set_a_limit_on_the_number_of_rejected_requests')
-            suspend_number_of_rejected_requests = frappe.db.get_single_value('PSA Settings', 'suspend_number_of_rejected_requests')
+        # else:
+        #     suspend_set_a_limit_on_the_number_of_requests = frappe.db.get_single_value('PSA Settings', 'suspend_set_a_limit_on_the_number_of_requests')
+        #     suspend_number_of_requests = frappe.db.get_single_value('PSA Settings', 'suspend_number_of_requests')
 
-            if suspend_set_a_limit_on_the_number_of_requests or suspend_set_a_limit_on_the_number_of_rejected_requests:
-                student_program_suspend_requests = frappe.get_all('Suspend Enrollment Request', filters={'program_enrollment': self.program_enrollment}, fields=['*'])
-                count_of_allowed = 0
-                count_of_rejected = 0
+        #     suspend_set_a_limit_on_the_number_of_rejected_requests = frappe.db.get_single_value('PSA Settings', 'suspend_set_a_limit_on_the_number_of_rejected_requests')
+        #     suspend_number_of_rejected_requests = frappe.db.get_single_value('PSA Settings', 'suspend_number_of_rejected_requests')
 
-                for request in student_program_suspend_requests:
-                    if "Approved by" in request.status and suspend_set_a_limit_on_the_number_of_requests:
-                        count_of_allowed += 1
-                        if count_of_allowed >= suspend_number_of_requests:
-                            frappe.throw(_("Can't add a suspend enrollment request, because you have been suspended! (Max of allowed suspend enrollment requests = ") + str(suspend_number_of_requests) + ")")
-                    elif "Rejected by" in request.status and suspend_set_a_limit_on_the_number_of_rejected_requests:
-                        count_of_rejected += 1
-                        if count_of_rejected >= suspend_number_of_rejected_requests:
-                            frappe.throw(_("Can't add a suspend enrollment request, because you requested more than limit: ") + str(suspend_number_of_rejected_requests) + _(" requests!"))
+        #     if suspend_set_a_limit_on_the_number_of_requests or suspend_set_a_limit_on_the_number_of_rejected_requests:
+        #         student_program_suspend_requests = frappe.get_all('Suspend Enrollment Request', filters={'program_enrollment': self.program_enrollment}, fields=['*'])
+        #         count_of_allowed = 0
+        #         count_of_rejected = 0
 
-            active_suspend = get_active_request("Suspend Enrollment Request", self.program_enrollment)
-            active_continue = get_active_request("Continue Enrollment Request", self.program_enrollment)
-            active_withdrawal = get_active_request("Withdrawal Request", self.program_enrollment)
+        #         for request in student_program_suspend_requests:
+        #             if "Approved by" in request.status and suspend_set_a_limit_on_the_number_of_requests:
+        #                 count_of_allowed += 1
+        #                 if count_of_allowed >= suspend_number_of_requests:
+        #                     frappe.throw(_("Can't add a suspend enrollment request, because you have been suspended! (Max of allowed suspend enrollment requests = ") + str(suspend_number_of_requests) + ")")
+        #             elif "Rejected by" in request.status and suspend_set_a_limit_on_the_number_of_rejected_requests:
+        #                 count_of_rejected += 1
+        #                 if count_of_rejected >= suspend_number_of_rejected_requests:
+        #                     frappe.throw(_("Can't add a suspend enrollment request, because you requested more than limit: ") + str(suspend_number_of_rejected_requests) + _(" requests!"))
 
-            if active_suspend:
-                url_of_active_suspend_request = '<a href="/app/suspend-enrollment-request/{0}" title="{1}">{2}</a>'.format(active_suspend.name, _("Click here to show request details"), active_suspend.name)
-                frappe.throw(
-                    _("Can't add a suspend enrollment request, because you have an active suspend enrollment request (") +
-                    url_of_active_suspend_request +
-                    _(") that is {0}!").format(active_suspend.status)
-                )
+        #     active_suspend = get_active_request("Suspend Enrollment Request", self.program_enrollment)
+        #     active_continue = get_active_request("Continue Enrollment Request", self.program_enrollment)
+        #     active_withdrawal = get_active_request("Withdrawal Request", self.program_enrollment)
 
-            elif active_continue:
-                url_of_active_continue_request = '<a href="/app/continue-enrollment-request/{0}" title="{1}">{2}</a>'.format(active_continue.name, _("Click here to show request details"), active_continue.name)
-                frappe.throw(
-                    _("Can't add a suspend enrollment request, because you have an active continue enrollment request (") +
-                    url_of_active_continue_request +
-                    _(") that is {0}!").format(active_continue.status)
-                )
+        #     if active_suspend:
+        #         url_of_active_suspend_request = '<a href="/app/suspend-enrollment-request/{0}" title="{1}">{2}</a>'.format(active_suspend.name, _("Click here to show request details"), active_suspend.name)
+        #         frappe.throw(
+        #             _("Can't add a suspend enrollment request, because you have an active suspend enrollment request (") +
+        #             url_of_active_suspend_request +
+        #             _(") that is {0}!").format(active_suspend.status)
+        #         )
 
-            elif active_withdrawal:
-                url_of_active_withdrawal_request = '<a href="/app/withdrawal-request/{0}" title="{1}">{2}</a>'.format(active_withdrawal.name, _("Click here to show request details"), active_withdrawal.name)
-                frappe.throw(
-                    _("Can't add a suspend enrollment request, because you have an active withdrawal request (") +
-                    url_of_active_withdrawal_request +
-                    _(") that is {0}!").format(active_withdrawal.status)
-                )
+        #     elif active_continue:
+        #         url_of_active_continue_request = '<a href="/app/continue-enrollment-request/{0}" title="{1}">{2}</a>'.format(active_continue.name, _("Click here to show request details"), active_continue.name)
+        #         frappe.throw(
+        #             _("Can't add a suspend enrollment request, because you have an active continue enrollment request (") +
+        #             url_of_active_continue_request +
+        #             _(") that is {0}!").format(active_continue.status)
+        #         )
+
+        #     elif active_withdrawal:
+        #         url_of_active_withdrawal_request = '<a href="/app/withdrawal-request/{0}" title="{1}">{2}</a>'.format(active_withdrawal.name, _("Click here to show request details"), active_withdrawal.name)
+        #         frappe.throw(
+        #             _("Can't add a suspend enrollment request, because you have an active withdrawal request (") +
+        #             url_of_active_withdrawal_request +
+        #             _(") that is {0}!").format(active_withdrawal.status)
+        #         )
 
     @staticmethod
     def send_suspend_enrollment_notification():
