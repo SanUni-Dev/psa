51cb2b7524309651d35082fde06f6af505ef9bb2
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-06-25 03:08:32 +0530

    Fix(Suspend): edited the code and message that change the status of program enrollment.

--- psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.py
+++ psa/psa/doctype/suspend_enrollment_request/suspend_enrollment_request.py
@@ -47,21 +47,15 @@ class SuspendEnrollmentRequest(Document):
                     if "Approved by" in request.status and suspend_set_a_limit_on_the_number_of_requests:
                         count_of_allowed += 1
                         if count_of_allowed >= suspend_number_of_requests:
-                            frappe.throw(_("Can't add a suspend enrollment request, because you have been suspended! (Max of allowed suspend enrollment requests = ") + str(suspend_number_of_requests) + ")")
+                            frappe.throw(_("Can't add a suspend enrollment request, because you have been suspended! (Max of allowed suspend enrollment requests = {0})").format(str(suspend_number_of_requests)))
                     elif "Rejected by" in request.status and suspend_set_a_limit_on_the_number_of_rejected_requests:
                         count_of_rejected += 1
                         if count_of_rejected >= suspend_number_of_rejected_requests:
-                            frappe.throw(_("Can't add a suspend enrollment request, because you requested more than limit: ") + str(suspend_number_of_rejected_requests) + _(" requests!"))
+                            frappe.throw(_("Can't add a suspend enrollment request, because you requested more than limit: {0} requests!").format(str(suspend_number_of_rejected_requests)))
 
-            active_request = check_active_request(
-                self.student,
-                self.program_enrollment,
-                ["Suspend Enrollment Request", "Continue Enrollment Request", "Withdrawal Request"]
-            )
+            active_request = check_active_request(self.student, self.program_enrollment, ["Suspend Enrollment Request", "Continue Enrollment Request", "Withdrawal Request"])
             if active_request:
                 url_of_active_request = '<a href="/app/{0}/{1}" title="{2}">{3}</a>'.format((active_request[0]).lower().replace(" ", "-"), active_request[1]['name'], _("Click here to show request details"), active_request[1]['name'])
                 frappe.throw(
-                    _("Can't add a suspend enrollment request, because you have an active {0} (").format(active_request[0]) +
-                    url_of_active_request +
-                    _(") that is {0}!").format(active_request[1]['status'])
+                    _("Can't add a suspend enrollment request, because you have an active {0} ({1}) that is {2}!").format(active_request[0], url_of_active_request, active_request[1]['status'])
                 )
