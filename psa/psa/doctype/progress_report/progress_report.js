4fee9979f8a32cc9024ea22f88b3a11f9f83d33c
proAhmedUcv <aalywsfy612@gmail.com>
2024-06-26 01:21:01 +0300

    Feat(Progress Report): fix the create report_for_student function to show the supervisor name

--- psa/psa/doctype/progress_report/progress_report.js
+++ psa/psa/doctype/progress_report/progress_report.js
@@ -1,8 +1,15 @@
+// // Copyright (c) 2024, Sana'a university and contributors
+// // For license information, please see license.txt
+
+ 
+
+
 // Copyright (c) 2024, Sana'a university and contributors
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Progress Report", {
-// 	refresh(frm) {
-
-// 	},
+//     refresh(frm) {
+//  
+//     },
 // });
+
