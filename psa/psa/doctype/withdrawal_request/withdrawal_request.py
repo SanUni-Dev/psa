445484788521fc763028419f4703b7528723ee27
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-06-28 03:52:38 +0530

    Feat(Withdrawal): implemented the new changes in validation using dynamic functions in the python file.

--- psa/psa/doctype/withdrawal_request/withdrawal_request.py
+++ psa/psa/doctype/withdrawal_request/withdrawal_request.py
@@ -4,7 +4,7 @@
 import frappe, json
 from frappe.model.document import Document
 from frappe import _
-from psa.api.psa_utils import get_active_request
+from psa.api.psa_utils import check_active_request, check_program_enrollment_status
 
 class WithdrawalRequest(Document):
     def on_submit(self):
@@ -24,37 +24,13 @@ class WithdrawalRequest(Document):
 
 
     def before_insert(self):
-        program_enrollment_status = frappe.get_doc('Program Enrollment', self.program_enrollment)
-
-        if program_enrollment_status.status ==" Withdrawn":
-            frappe.throw(_("Can't add a withdrawal  request, because current status is withdrawn!"))
-        
-        else:
-            active_suspend = get_active_request("Suspend Enrollment Request", self.program_enrollment)
-            active_continue = get_active_request("Continue Enrollment Request", self.program_enrollment)
-            active_withdrawal = get_active_request("Withdrawal Request", self.program_enrollment)
-
-            if active_suspend:
-                url_of_active_suspend_request = '<a href="/app/suspend-enrollment-request/{0}" title="{1}">{2}</a>'.format(active_suspend.name, _("Click here to show request details"), active_suspend.name)
+        program_enrollment_status = check_program_enrollment_status(self.program_enrollment, ['Suspended', 'Continued'], ['Withdrawn', 'Graduated', 'Transferred'])
+        if not program_enrollment_status[0]:
+            frappe.throw(_("Can't add a withdrawal request, because current status is {0}!").format(program_enrollment_status[1]))
+        elif program_enrollment_status[0]:
+            active_request = check_active_request(self.student, self.program_enrollment, ["Suspend Enrollment Request", "Continue Enrollment Request", "Withdrawal Request"])
+            if active_request:
+                url_of_active_request = '<a href="/app/{0}/{1}" title="{2}">{3}</a>'.format((active_request[0]).lower().replace(" ", "-"), active_request[1]['name'], _("Click here to show request details"), active_request[1]['name'])
                 frappe.throw(
-                    _("Can't add a withdrawal request, because you have an active suspend enrollment request (") +
-                    url_of_active_suspend_request +
-                    _(") that is {0}!").format(active_suspend.status)
-                )
-
-            elif active_continue:
-                url_of_active_continue_request = '<a href="/app/continue-enrollment-request/{0}" title="{1}">{2}</a>'.format(active_continue.name, _("Click here to show request details"), active_continue.name)
-                frappe.throw(
-                    _("Can't add a withdrawal request, because you have an active continue enrollment request (") +
-                    url_of_active_continue_request +
-                    _(") that is {0}!").format(active_continue.status)
-                )
-            
-            elif active_withdrawal:
-                url_of_active_withdrawal_request = '<a href="/app/withdrawal-request/{0}" title="{1}">{2}</a>'.format(active_withdrawal.name, _("Click here to show request details"), active_withdrawal.name)
-                frappe.throw(
-                    _("Can't add a withdrawal request, because you have an active withdrawal request (") +
-                    url_of_active_withdrawal_request +
-                    _(") that is {0}!").format(active_withdrawal.status)
-                )
-                
+                    _("Can't add a withdrawal request, because you have an active {0} ({1}) that is {2}!").format(active_request[0], url_of_active_request, active_request[1]['status'])
+                )                
