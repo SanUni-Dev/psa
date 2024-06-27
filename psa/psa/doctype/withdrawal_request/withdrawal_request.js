f77946c77fbe8499fd933bf7291b34a327656d1a
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-06-28 05:29:11 +0530

    Feat(Withdrawal): implemented the new changes in validation using dynamic functions in the java script file.

--- psa/psa/doctype/withdrawal_request/withdrawal_request.js
+++ psa/psa/doctype/withdrawal_request/withdrawal_request.js
@@ -166,43 +166,29 @@ frappe.ui.form.on("Withdrawal Request", {
                         var array_of_label = [__("Program Abbreviation"), __("Faculty"), __("Faculty Department"), __("Status")];
                         var array_of_value = [program_abbreviation, faculty, faculty_department, status];
                         psa_utils.format_multi_html_field(frm, "student_html2", array_of_label, array_of_value);
+
+                        psa_utils.check_program_enrollment_status(frm.doc.program_enrollment, ['Suspended', 'Continued'], ['Withdrawn', 'Graduated', 'Transferred'],
+                            function(program_enrollment_status) {
+                                if (!program_enrollment_status[0]) {
+                                    frm.set_intro((__("Can't add a withdrawal request, because current status is {0}!", [program_enrollment_status[1]])), 'red');
+                                }
+                                else if (program_enrollment_status[0]) {
+                                    psa_utils.check_active_request(frm.doc.student, frm.doc.program_enrollment, ['Withdrawal Request', 'Suspend Enrollment Request', 'Continue Enrollment Request'],
+                                        function (active_request) {
+                                            if (active_request) {
+                                                var url_of_active_request = `<a href="/app/${active_request[0].toLowerCase().replace(/\s+/g, "-")}/${active_request[1]['name']}" title="${__("Click here to show request details")}"> ${active_request[1]['name']} </a>`;
+                                                frm.set_intro((__(`Can't add a withdrawal request, because you have an active {0} ({1}) that is {2}!`, [active_request[0], url_of_active_request, active_request[1]['status']])), 'red');
+                                            }
+                                            else {
+                                                frm.set_intro((__(`Current status is {0}.`, [program_enrollment_status[1]])), 'green');
+                                            }
+                                        }
+                                    );
+                                }
+                            }
+                        );
                     });
                 });
-
-                if (status == "Withdrawn") {
-                    frm.set_intro((__(`Can't add Withdrawnal request, because current status is ${status}!`)), 'red');
-                }
-
-                else {
-                    psa_utils.get_active_request("Withdrawal Request", frm.doc.program_enrollment, function (doc) {
-                        if (doc) {
-                            frm.set_intro('');
-                            var url_of_active_request = `<a href="/app/withdrawal-request/${doc.name}" title="${__("Click here to show request details")}"> ${doc.name} </a>`;
-                            frm.set_intro((__(`Can't add a Withdrawal request, because you have an active withdrawal request (`) + url_of_active_request + __(`) that is ${doc.status}!`)), 'red');
-                        }
-                        else {
-                            psa_utils.get_active_request("Suspend Enrollment Request", frm.doc.program_enrollment, function (doc) {
-                                if (doc) {
-                                    frm.set_intro('');
-                                    var url_of_active_request = `<a href="/app/suspend-enrollment-request/${doc.name}" title="${__("Click here to show request details")}"> ${doc.name} </a>`;
-                                    frm.set_intro((__(`Can't add a Withdrawal request, because you have an active suspend enrollment request (`) + url_of_active_request + __(`) that is ${doc.status}!`)), 'red');
-                                }
-                                else {
-                                    psa_utils.get_active_request("Continue Enrollment Request", frm.doc.program_enrollment, function (doc) {
-                                        if (doc) {
-                                            frm.set_intro('');
-                                            var url_of_active_request = `<a href="/app/continue-enrollment-request/${doc.name}" title="${__("Click here to show request details")}"> ${doc.name} </a>`;
-                                            frm.set_intro((__(`Can't add a Withdrawal request, because you have an active continue enrollment request (`) + url_of_active_request + __(`) that is ${doc.status}!`)), 'red');
-                                        }
-                                        else if (status == "Continued" || status == "Suspended") {
-                                            frm.set_intro((__(`Current status is ${status}.`)), 'green');
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
-                }
             });
         }
         else {
