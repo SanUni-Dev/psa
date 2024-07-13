db2f49268a18f10fa5a93db6fec684f0784e46cd
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-07-14 02:22:09 +0530

    Feat(Progress Report): implemented the new changes in validation using dynamic functions in the python and java script files.

--- psa/psa/doctype/progress_report/progress_report.js
+++ psa/psa/doctype/progress_report/progress_report.js
@@ -1,15 +1,45 @@
 // // Copyright (c) 2024, Sana'a university and contributors
-// // For license information, please see license.txt
-
- 
-
-
 // Copyright (c) 2024, Sana'a university and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Progress Report", {
-//     refresh(frm) {
-//  
-//     },
-// });
+frappe.ui.form.on("Progress Report", {
+    refresh(frm) {
+        setTimeout(() => {
+            frm.page.actions.find(`[data-label='Help']`).parent().parent().remove();
+        }, 500);
+    },
 
+    onload(frm) {
+        if (frm.is_new() && frappe.user_roles.includes("Student")) {
+            psa_utils.set_student_for_current_user(frm, "student", function () {
+                psa_utils.set_program_enrollment_for_current_user(frm, "program_enrollment");
+            });
+        }
+    },
+
+    program_enrollment(frm) {
+        frm.set_intro('');
+        if (frm.doc.program_enrollment) {
+            psa_utils.check_program_enrollment_status(frm.doc.program_enrollment, ['Continued'], ['Suspended', 'Withdrawn', 'Graduated', 'Transferred'],
+                function (program_enrollment_status) {
+                    if (!program_enrollment_status[0]) {
+                        frm.set_intro((__("Can't add a progress report, because current status is {0}!", [program_enrollment_status[1]])), 'red');
+                    }
+                    else if (program_enrollment_status[0]) {
+                        frm.set_intro((__(`Current status is {0}.`, [program_enrollment_status[1]])), 'green');
+                    }
+                }
+            );
+        }
+    },
+
+    student(frm) {
+        if (frm.doc.student) {
+            psa_utils.set_program_enrollment_for_student(frm, "program_enrollment", frm.doc.student);
+        }
+        else {
+            frm.set_value("program_enrollment", "");
+            refresh_field("program_enrollment");
+        }
+    },
+});
--- psa/psa/doctype/progress_report/progress_report.py
+++ psa/psa/doctype/progress_report/progress_report.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, Sana'a university and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe, json
 from frappe.model.document import Document
+from frappe import _
+from psa.api.psa_utils import check_program_enrollment_status
 
 
 class ProgressReport(Document):
-	pass
+	def before_insert(self):
+		program_enrollment_status = check_program_enrollment_status(self.program_enrollment, ['Continued'], ['Suspended', 'Withdrawn', 'Graduated', 'Transferred'])
+		if not program_enrollment_status[0]:
+			frappe.throw(_("Can't add a progress report, because current status is {0}!").format(program_enrollment_status[1]))
