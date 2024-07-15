92e341901b940635acb654a759fb33a7a7d5b117
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-07-15 08:07:52 +0530

    Feat(Progress Report): edited the fields, properties, and permissions, and added code to get the researcher meetings that are in the period of the report and insert them to the child table, and added validation for dates in py and js.

--- psa/psa/doctype/progress_report/progress_report.js
+++ psa/psa/doctype/progress_report/progress_report.js
@@ -1,4 +1,3 @@
-// // Copyright (c) 2024, Sana'a university and contributors
 // Copyright (c) 2024, Sana'a university and contributors
 // For license information, please see license.txt
 
@@ -7,6 +6,15 @@ frappe.ui.form.on("Progress Report", {
         setTimeout(() => {
             frm.page.actions.find(`[data-label='Help']`).parent().parent().remove();
         }, 500);
+
+        if (!frm.is_new()) {
+            if (frm.doc.meetings.length == 0) {
+                $(frm.fields_dict["meetings_status"].wrapper).html(`<p>${__("There is not any meeting in the period.")}</p>`);
+            }
+            else {
+                $(frm.fields_dict["meetings_status"].wrapper).html(``);
+            }
+        }
     },
 
     onload(frm) {
@@ -21,9 +29,18 @@ frappe.ui.form.on("Progress Report", {
 
     program_enrollment(frm) {
         frm.set_intro('');
+        frm.set_value("supervisor", "");
+        refresh_field("supervisor");
+        frm.set_value("meetings", null);
+        refresh_field("meetings");
+
         if (frm.doc.program_enrollment) {
-            frm.set_value("supervisor", "");
             psa_utils.set_student_supervisor_for_student_and_program_enrollment(frm, "supervisor", frm.doc.student, frm.doc.program_enrollment);
+            
+            if (frm.doc.student && frm.doc.program_enrollment && frm.doc.from_date && frm.doc.to_date) {
+                psa_utils.set_researcher_meetings(frm, "meetings", frm.doc.student, frm.doc.program_enrollment, frm.doc.from_date, frm.doc.to_date);
+            }
+            
             psa_utils.check_program_enrollment_status(frm.doc.program_enrollment, ['Continued'], ['Suspended', 'Withdrawn', 'Graduated', 'Transferred'],
                 function (program_enrollment_status) {
                     if (!program_enrollment_status[0]) {
@@ -52,10 +69,6 @@ frappe.ui.form.on("Progress Report", {
                 }
             );
         }
-        else {
-            frm.set_value("supervisor", "");
-            refresh_field("supervisor");
-        }
     },
 
     student(frm) {
@@ -65,8 +78,28 @@ frappe.ui.form.on("Progress Report", {
         else {
             frm.set_value("program_enrollment", "");
             refresh_field("program_enrollment");
-            frm.set_value("supervisor", "");
-            refresh_field("supervisor");
+        }
+    },
+
+    from_date(frm) {
+        frm.set_value("meetings", null);
+
+        if (frm.doc.student && frm.doc.program_enrollment && frm.doc.from_date && frm.doc.to_date) {
+            psa_utils.set_researcher_meetings(frm, "meetings", frm.doc.student, frm.doc.program_enrollment, frm.doc.from_date, frm.doc.to_date);
+        }
+    },
+
+    to_date(frm) {
+        frm.set_value("meetings", null);
+
+        if (frm.doc.student && frm.doc.program_enrollment && frm.doc.from_date && frm.doc.to_date) {
+            psa_utils.set_researcher_meetings(frm, "meetings", frm.doc.student, frm.doc.program_enrollment, frm.doc.from_date, frm.doc.to_date);
+        }
+    },
+
+    go_to_meetings(frm) {
+        if(frm.doc.meetings) {
+            frm.scroll_to_field("meetings", focus = true);
         }
     },
 });
