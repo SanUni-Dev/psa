9e217e4fd0f129bad4aa37e3884f18e1078c1d30
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-08-02 09:28:20 +0530

    refactor(researcher-meeting): refactor doctype fields and validation.

--- psa/psa/doctype/researcher_meeting/researcher_meeting.js
+++ psa/psa/doctype/researcher_meeting/researcher_meeting.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2024, Sana'a university and contributors
 // For license information, please see license.txt
 
+
 frappe.ui.form.on('Researcher Meeting', {
     setup: function (frm) {
         frm.set_query("student_supervisor", "meeting_with", function (doc, cdt, cdn) {
@@ -16,9 +17,11 @@ frappe.ui.form.on('Researcher Meeting', {
     },
 
     refresh(frm) {
-        setTimeout(() => {
-            frm.page.actions.find(`[data-label='Help']`).parent().parent().remove();
-        }, 500);
+        $(frm.fields_dict["information"].wrapper).html("");
+
+        if (frm.doc.student && frm.doc.program_enrollment) {
+            psa_utils.set_program_enrollment_information(frm, "information", frm.doc.student, frm.doc.program_enrollment);
+        }
     },
 
     onload(frm) {
@@ -33,30 +36,38 @@ frappe.ui.form.on('Researcher Meeting', {
 
     program_enrollment(frm) {
         frm.set_intro('');
+        frm.set_value("student_research", "");
+        refresh_field("student_research");
+        
         if (frm.doc.program_enrollment) {
-            frm.set_value("student_research", "");
+            psa_utils.set_program_enrollment_information(frm, "information", frm.doc.student, frm.doc.program_enrollment);
             psa_utils.set_student_research_for_student_and_program_enrollment(frm, "student_research", frm.doc.student, frm.doc.program_enrollment);
+
             psa_utils.check_program_enrollment_status(frm.doc.program_enrollment, ['Continued'], ['Suspended', 'Withdrawn', 'Graduated', 'Transferred'],
                 function (program_enrollment_status) {
                     if (!program_enrollment_status[0]) {
+                        frm.set_intro('');
                         frm.set_intro((__("Can't add a researcher meeting, because current status is {0}!", [program_enrollment_status[1]])), 'red');
                     }
                     else if (program_enrollment_status[0]) {
-                        frappe.db.get_single_value('PSA Settings', 'check_active_requests_before_insert').then((check_active_requests_before_insert) => {
+                        psa_utils.get_single_value('PSA Settings', 'check_active_requests_before_insert', function (check_active_requests_before_insert) {
                             if (check_active_requests_before_insert) {
                                 psa_utils.check_active_request(frm.doc.student, frm.doc.program_enrollment, ['Suspend Enrollment Request', 'Continue Enrollment Request', 'Withdrawal Request'],
                                     function (active_request) {
                                         if (active_request) {
+                                            frm.set_intro('');
                                             var url_of_active_request = `<a href="/app/${active_request[0].toLowerCase().replace(/\s+/g, "-")}/${active_request[1]['name']}" title="${__("Click here to show request details")}"> ${active_request[1]['name']} </a>`;
                                             frm.set_intro((__(`Can't add a researcher meeting, because you have an active {0} ({1}) that is {2}!`, [active_request[0], url_of_active_request, active_request[1]['status']])), 'red');
                                         }
                                         else {
+                                            frm.set_intro('');
                                             frm.set_intro((__(`Current status is {0}.`, [program_enrollment_status[1]])), 'green');
                                         }
                                     }
                                 );
                             }
                             else {
+                                frm.set_intro('');
                                 frm.set_intro((__(`Current status is {0}.`, [program_enrollment_status[1]])), 'green');
                             }
                         });
@@ -65,6 +76,7 @@ frappe.ui.form.on('Researcher Meeting', {
             );
         }
         else {
+            $(frm.fields_dict["information"].wrapper).html("");
             frm.set_value("student_research", "");
             refresh_field("student_research");
             frm.set_value("meeting_with", null);
@@ -79,8 +91,6 @@ frappe.ui.form.on('Researcher Meeting', {
         else {
             frm.set_value("program_enrollment", "");
             refresh_field("program_enrollment");
-            frm.set_value("student_research", "");
-            refresh_field("student_research");
         }
     },
 });
