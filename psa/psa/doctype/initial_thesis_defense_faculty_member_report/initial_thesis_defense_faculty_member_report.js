ffac07a0f309e93dfbeb6d5b7c5c1ecc6d00467f
EsmailEbrahim <esmailebraheem771@gmail.com>
2024-08-24 03:37:21 +0530

    feat(initial-thesis-defense): create request doctypes

---
+++ psa/psa/doctype/initial_thesis_defense_faculty_member_report/initial_thesis_defense_faculty_member_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Sana'a university and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Initial Thesis Defense Faculty Member Report", {
+// 	refresh(frm) {
+
+// 	},
+// });
